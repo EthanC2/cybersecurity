9eb3deb11996585c767605a6844570f5ede3570f
EthanC2 <ethanrcox@gmail.com>
2022-11-08 00:20:49 -0500

    initial commit

---
+++ src/cryptography/caesar.rs
@@ -0,0 +1,9 @@
+pub fn encrypt<S>(plaintext: S) -> String
+where S: AsRef<str> {
+    "".to_string()
+}
+
+pub fn decrypt<S>(ciphertext: S) -> String
+where S: AsRef<str> {
+    "".to_string()
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+mod cryptography;
+
+fn main() {
+    println!("Hello, world!");
+}
